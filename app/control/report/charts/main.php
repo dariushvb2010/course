134ec5f2e05b07db25c32ce665bb0751501f838f
morteza kavakebi <m.kavakebi@gmail.com>
2012-02-24 16:43:34 +0330

    this is new chart structure

--- app/control/report/charts/main.php
+++ app/control/report/charts/main.php
@@ -3,13 +3,44 @@ class ReportChartsMainController extends JControl
 {
 	function Start()
 	{
-		$days=30;
-		$r=ORM::Query(new ReviewProgressStart)->DailyStart($days);
-		$this->firstday=(time()-24*60*60*$days)*1000;
-		foreach ($r as $key =>$value){
-			$daily1[]=$value['count'];
+		$ChartTypeArray=array('daftar_cotag','percentage','yy');
+		
+		$ChartType=$ChartTypeArray[0];
+		if(isset($_GET['charttype'])){
+			if($_GET['charttype']>=0 AND $_GET['charttype']<count($ChartTypeArray)){
+				$ChartType=$ChartTypeArray[$_GET['charttype']];
+			}
 		}
-		$this->daily1=$daily1;
+		
+		$this->ChartType=$ChartType;
+		$this->ConfigFileName=$ChartType;
+		
+		switch ($ChartType){
+			case 'daftar_cotag':
+				$days=30;
+				$r=ORM::Query(new ReviewProgressStart)->DailyStart($days);
+				$this->firstday=(time()-24*60*60*$days)*1000;
+				foreach ($r as $key =>$value){
+					$daily1[]=$value['count'];
+				}
+				$this->daily1=$daily1;
+				break;
+			case 'percentage':
+				$percentarray=array(
+					'خطا' =>50 ,
+					'کسر دریافتی'=>26,
+					'قطع مرور زمان'=>8,
+					'غیره'=>2);
+				foreach ($percentarray as $key=> $value){
+					$out_ar[]="['{$key}' ,{$value}]";
+				}
+				$this->percentarray=$out_ar;
+				
+				break;
+			
+		}
+		
+		
 		$this->Error=$Error;
 		if(Count($Error))
 			$this->Result=false;
