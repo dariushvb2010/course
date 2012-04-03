09a9528671ede533b28d03149c0dcaf332d87415
sonukesh <dariush@ubuntu.ubuntu-domain>
2012-04-03 09:03:49 +0300

    fixed

---
+++ app/control/desk/alarm.php
@@ -0,0 +1,15 @@
+<?php 
+class DeskAlarmController extends JControl
+{
+	function Start()
+	{
+		$this->User=MyUser::CurrentUser();
+		if($this->User && $this->User->Group())
+			$this->GroupTitle=$this->User->Group()->PersianTitle();
+		
+		$this->Alarm_Personal=ORM::Query("Alarm")->CurrentUserAlarms_Personal(0,9999999);
+		$this->Alarm_Group=ORM::Query("Alarm")->CurrentUserAlarms_Group(0,9999999);
+		
+		return $this->Present();
+	}
+}
