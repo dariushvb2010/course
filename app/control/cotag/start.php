60c27ddceff93b83fffba8a181094fcfb90de56c
dariush <dariush@dariush-pc.(none)>
2012-08-15 10:00:40 +0430

    lkj

--- app/control/cotag/start.php
+++ app/control/cotag/start.php
@@ -5,21 +5,32 @@ class CotagStartController extends JControl
 	function Start()
 	{
 		j::Enforce("CotagBook");
+		return $this->SemiStart(MyGroup::CotagBook());
 		
 		
+	}
+	/**
+	 * i write this function to inherite it in other controllers:[ArchiveStart, RakedStart, CorrespondecsStart]
+	 * @param $gname string GroupName and also ProgressName like :[Start, Archive, Raked, Correspondence]
+	 */
+	public function SemiStart( MyGroup $startGroup)
+	{
 		if (count($_POST))
 		{
+// 			$startGroup = MyGroup::GetGroup($gname);
+// 			if($startGroup==null)
+// 				throw new Exception("startGroup cannot be null. we dont have a group name [".$gname."]");
 			$IsPrint=isset($_POST['print']) ? true : false;
 			$Cotag=$_POST['Cotag'];
 			$this->Cotag=$Cotag;
-
+		
 			if(empty($Error)){
-				$Res=ORM::Query("ReviewProgressStart")->AddToFile($Cotag,$IsPrint);
+				$Res=ORM::Query("ReviewProgressStart")->AddToFile($Cotag,$IsPrint,$startGroup);
 				if(is_string($Res))
 				{
 					$Error[]=$Res;
 				}
-				else 
+				else
 				{
 					$this->Result = " اظهارنامه با شماره کوتاژ ".v::bgc($Cotag)." با موفقیت وصول گردید. ";
 				}
@@ -34,8 +45,7 @@ class CotagStartController extends JControl
 		$this->IsPrint=$IsPrint;
 		
 		if (count($Error)) $this->Result=false;
-		return $this->Present();
+		return $this->present("","cotag.start");
 	}
-	
 	
 }
