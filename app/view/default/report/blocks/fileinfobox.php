5ce98dbdbf0ccde4a185bd4707cc616982406aba
unknown <m.kavakebi@gmail.com>
2012-08-22 23:19:44 +0430

    k

--- app/view/default/report/blocks/fileinfobox.php
+++ app/view/default/report/blocks/fileinfobox.php
@@ -24,7 +24,7 @@
 		<p class="left">وضعیت: <?php echo $this->File->State();?></p>
 		<p class="right">شماره کوتاژ: <?php echo v::Filecuc($this->File,"Cb,Sb,link");?></p>
 		<?php //echo v::bbc($this->File->Cotag());?>
-		<p class="left">سریال اظهارنامه: <?php echo $this->File->BarSerial();?></p>
+		<p class="left">سریال اظهارنامه: <?php echo ($this->File->BarSerial()?$this->File->BarSerial():'-');?></p>
 		<p class="right">کد گمرک: <?php echo $this->File->GateCode();?></p>
 		<p class="right">تاریخ وصول: <span dir=ltr><?php echo $this->File->CreateTime();?></span></p>
 		<p class="right">شماره کلاسه: <span dir=ltr><?php echo ($this->File->GetClass()?$this->File->GetClass():'-');?></span></p>
