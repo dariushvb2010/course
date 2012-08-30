725fa34ced34bae247ce68638ea117ac639796d8
root <root@bazbini.(none)>
2012-08-30 10:08:49 +0430

    Revert "alarm fixed"
    This reverts commit 5772494f2e0c194247448fe530229f4f1f6a055e.

--- app/control/config/alarm.php
+++ app/control/config/alarm.php
@@ -3,6 +3,7 @@ class ConfigAlarmController extends AlarmController
 {
 	function Start()
 	{
+		file_put_contents("a.txt", "hi", FILE_APPEND | LOCK_EX);
 		if(count($_POST) AND !isset($_POST['AutoEditSave']))
 		{
 			
@@ -44,7 +45,7 @@ class ConfigAlarmController extends AlarmController
 		$al->SetHeader("ID", "شناسه",false,false,false);
 		$al->SetHeader("Title", "عنوان");
 		$al->SetHeader("Context", "متن");
-		$al->SetHeader("MoratoriumInDays", "مهلت");
+		$al->SetHeader("MoratoriumToDays", "مهلت");
 		$al->SetHeader("Context", "متن");
 		$al->SetHeader("Style", "نوع", false,false,false);
 		$al->EnableEdit("ویرایش","ID");
