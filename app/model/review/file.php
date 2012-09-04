0524b6bb245d5096ff5c99a2d87c1c49cb917bf5
unknown <m.kavakebi@gmail.com>
2012-09-04 20:50:17 +0430

    dd

--- app/model/review/file.php
+++ app/model/review/file.php
@@ -433,6 +433,7 @@ class ReviewFileRepository extends EntityRepository
 		}else{
 			$where='';
 		}
+		echo $where;
 		//----------------------------------------------------------
 		if($Pagination=='CountAll'){
 			$QueryStr="SELECT count(F) as result FROM ReviewFile AS F ".$where;
