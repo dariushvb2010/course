e5307d7624d924fb904f48d07f9529c0e93cffa8
invisibleboy <taram.mohammad@gmail.com>
2012-09-09 07:15:02 +0430

    asycuda data table

--- app/model/p.php
+++ app/model/p.php
@@ -1,22 +1,37 @@
 <?php
+
 /**
  * for constants
  * @author dariush
  *
  */
-class p{
-	const Archive = 'بایگانی بازبینی';
-	const CotagBook = 'دفتر کوتاژ';
-	const Raked = 'بایگانی راکد';
-	const Mokatebat = 'مکاتبات';
-	const Cotag = 'کوتاژ';
-	const File = 'اظهارنامه';
-	const Cat = 'شماره طبقه بندی';
-	
-	const setad = 'دفاتر ستادی';
-	const commission = 'کمیسیون';
-	const appeals = 'کمیسیون تجدید نظر';
-	const desc = 'توضیحات';
-	const MailNum = 'شماره نامه';
-	const P7 = 'ماده ۷';
+class p {
+
+    const Archive = 'بایگانی بازبینی';
+    const CotagBook = 'دفتر کوتاژ';
+    const Raked = 'بایگانی راکد';
+    const Mokatebat = 'مکاتبات';
+    const Cotag = 'کوتاژ';
+    const File = 'اظهارنامه';
+    const Cat = 'شماره طبقه بندی';
+    const setad = 'دفاتر ستادی';
+    const commission = 'کمیسیون';
+    const appeals = 'کمیسیون تجدید نظر';
+    const desc = 'توضیحات';
+    const MailNum = 'شماره نامه';
+    const P7 = 'ماده ۷';
+    const RegTime           ="تاریخ کوتاژ"          ;
+    const Masir = "مسیر";
+    const OwnerName = "نام صاحب کالا";
+    const OwnerCoding = "کدینگ صاحب کالا";
+    const DeclarantName = "نام اظهارکننده";
+    const DeclarantCoding = "کدینگ اظهار کننده";
+    const Karshenas_salon = "کارشناس سالن";
+    const Arzyab = "ارزیاب";
+    const Karshenas_arzesh = "کارشناس ارزش";
+    const ArzType = "نوع ارز";
+    const ArzNerkh = "نرخ ارز";
+    const RialPrice = "ارزش به ریال";
+    const TotalTaxes = "مجموع عوارض";
+
 }
