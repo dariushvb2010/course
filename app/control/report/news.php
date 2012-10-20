e8e7e2501a5f5ee810c427c0113d0f0b6a3c9492
dariush <dariush@dariush-pc.(none)>
2012-10-20 14:51:35 +0330

    news

--- app/control/report/news.php
+++ app/control/report/news.php
@@ -3,10 +3,10 @@ class ReportNewsController extends BaseControllerClass
 {
     function Start ()
     {
-    	$this->News = ORM::Query('News')->GetAllArray();
-//     	$News[] = 'زمان و مکان کلاس متعاقبا اعلام خواهد شد.';
-//     	$News[] = 'ارایه دهندگان: محمدکاظم تارم - داریوش جعفری دانشجویان ترم هفت کارشناسی';
-//     	$this->News = $News;
+    	$News = ORM::Query('News')->GetAllArray();
+    	$News[] = array('Description'=>'ارایه دهندگان: محمدکاظم تارم - داریوش جعفری دانشجویان ترم هفت کارشناسی');
+    	$News[] = array('Description'=>'زمان و مکان کلاس متعاقبا اعلام خواهد شد.');
+    	$this->News = $News;
     	return $this->Present();
     }
     
