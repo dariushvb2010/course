547286d05a80fc5316fbffb5a9779ebc57842805
dariush <dariush@dariush-pc.(none)>
2012-10-21 16:31:29 +0330

    hi

--- app/control/report/news.php
+++ app/control/report/news.php
@@ -5,7 +5,8 @@ class ReportNewsController extends BaseControllerClass
     {
     	$News = ORM::Query('News')->GetAllArray();
     	$News[] = array('Description'=>'ارایه دهندگان: محمدکاظم تارم - داریوش جعفری دانشجویان ترم هفت کارشناسی');
-    	$News[] = array('Description'=>'زمان و مکان کلاس متعاقبا اعلام خواهد شد.');
+    	$News[] = array('Description'=>'زمان و مکان کلاس: شنبه ها ۱۶-۱۴:۳۰ کلاس ۱۰۳.');
+    	$News[] = array('Description'=>'اولین جلسه : شنبه ۶ آبان');
     	$this->News = $News;
     	return $this->Present();
     }
